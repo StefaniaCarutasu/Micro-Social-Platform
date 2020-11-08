43d6473d7ae97bf13c85162ac51755cf7c92d7b9
Madalina-Nicolescu <62146366+Madalina-Nicolescu@users.noreply.github.com>
2020-11-08 22:54:10 +0200

    CRUD Comment

--- BDProiect/Controllers/CommentsController.cs
+++ BDProiect/Controllers/CommentsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BDProiect.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,66 @@ namespace BDProiect.Controllers
 {
     public class CommentsController : Controller
     {
+
+        private Models.AppContext db = new Models.AppContext();
         // GET: Comments
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult New(Comment comment)
+        {
+            comment.Date = DateTime.Now;
+            try
+            {
+                db.Comments.Add(comment);
+                db.SaveChanges();
+                return Redirect("/Posts/Show/" + comment.PostId);
+            }
+            catch(Exception e)
+            {
+                ViewBag.ErrorMessage = e.Message;
+                return View("Error");
+            }
+        }
+
+        [HttpDelete]
+
+        public ActionResult Delete(int id)
+        {
+            Comment comment = db.Comments.Find(id);
+            db.Comments.Remove(comment);
+            db.SaveChanges();
+            return Redirect("/Posts/Show/" + comment.PostId);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Comment comment = db.Comments.Find(id);
+            ViewBag.Comment = comment;
+            return View();
+        }
+
+        [HttpPut]
+        public ActionResult Edit(int id, Comment reqComment)
+        {
+            try
+            {
+                Comment comm = db.Comments.Find(id);
+                if(TryUpdateModel(comm))
+                {
+                    comm.Content = reqComment.Content;
+                    db.SaveChanges();
+                }
+                return Redirect("/Posts/Show/" + comm.PostId);
+            }
+            catch(Exception e)
+            {
+                return View();
+            }
+        }
+
     }
 }
