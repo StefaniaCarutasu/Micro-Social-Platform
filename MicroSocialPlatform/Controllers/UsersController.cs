b5dfbd7797812e7e2e114c0a8799011201a04c2f
Stefania Carutasu <62119841+StefaniaCarutasu@users.noreply.github.com>
2020-12-13 17:08:56 +0200

    Add UsersController

---
+++ MicroSocialPlatform/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using MicroSocialPlatform.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MicroSocialPlatform.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        private ApplicationDbContext db = ApplicationDbContext.Create();
+        // GET: Users
+        public ActionResult Index()
+        {
+            var users = from user in db.Users
+                        orderby user.UserName
+                        select user;
+
+            ViewBag.UsersList = users;
+            return View();
+        }
+    }
+}
